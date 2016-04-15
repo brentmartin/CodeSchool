36a472cd204500a8ff460846179528ead54755d7
Brent Martin <bmart35@gmail.com>
2016-04-14 22:17:59 -0500

    Refactored by defining variable tour (3.19)

--- Try_jQuery/Lesson-3/application.js
+++ Try_jQuery/Lesson-3/application.js
@@ -1,8 +1,9 @@
 $(document).ready(function() {
   $('button').on('click', function() {
-    var discount = $(this).closest('.tour').data('discount');
-    var message = $('<span>Call 1-555-jquery-air for a $'+discount+' discount.</span>');
-    $(this).closest('.tour').append(message);
+    var tour = $(this).closest('.tour');
+    var discount = tour.data('discount');
+    var message = $('<span>Call 1-555-jquery-air for a $' + discount + ' discount.</span>');
+    tour.append(message);
     $(this).remove();
   });
 });
