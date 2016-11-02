f00698ddce9934bccc789773b2ef3f891d530062
Brent Martin <bmart35@gmail.com>
2016-11-01 23:56:46 -0500

    add in some conditionals for isAllowed() method

--- Golang/On_Track_With_Golang/Lesson4/chat/main.go
+++ Golang/On_Track_With_Golang/Lesson4/chat/main.go
@@ -10,10 +10,29 @@ type guestConnection struct {
 	userName string
 }
 
+func (g guestConnection) isAllowed() bool {
+	return !isIPBlocked(g.ip) && g.userName != "Darth Vader"
+}
+
 func main() {
 	ip := util.GetGuestIP()
 	userName := "Kerry"
-	gConn := guestConnection{ip: ip, userName: userName}
 
-	fmt.Println(gConn)
+	gConn := guestConnection{ip: ip, userName: userName}
+	isAllowedStatus := gConn.isAllowed()
+	fmt.Println(isAllowedStatus)
+}
+
+func isIPBlocked(ip string) bool {
+	blockedIPs := getBlockedIPs()
+	for _, blockedIP := range blockedIPs {
+		if blockedIP == ip {
+			return true
+		}
+	}
+	return false
+}
+
+func getBlockedIPs() []string {
+	return []string{"192.168.0.17", "192.168.0.18", "192.168.0.19", "192.168.0.20"}
 }
