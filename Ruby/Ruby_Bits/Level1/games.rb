609e9874df9fc98e68880d4c3149f2561282807f
Brent Martin <bmart35@gmail.com>
2016-11-12 23:45:24 -0600

    used and to double up the conditional criteria

--- Ruby/Ruby_Bits/Level1/games.rb
+++ Ruby/Ruby_Bits/Level1/games.rb
@@ -1,8 +1,8 @@
-search = "Contra"
+search = "Super Mario Bros."
 games = ["Super Mario Bros.", "Contra", "Metroid", "Mega Man 2"]
-search_index = games.find_index(search)
-if search_index
-  puts "Game #{search} Found: #{games[search_index]} at index #{search_index}."
-else
-  puts "Game #{search} not found."
+matched_games = games.grep(Regexp.new(search))
+
+# Found an exact match
+if matched_games.length > 0 && matched_games.include?(search)
+  puts "Game #{search} found."
 end
