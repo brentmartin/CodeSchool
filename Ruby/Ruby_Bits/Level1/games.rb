6d99ffe4e3571eede8d06a64868b29ed561f0fc8
Brent Martin <bmart35@gmail.com>
2016-11-15 22:56:24 -0600

    completed Level1

--- Ruby/Ruby_Bits/Level1/games.rb
+++ Ruby/Ruby_Bits/Level1/games.rb
@@ -1,3 +1,6 @@
 def search_index(games, search_term)
   games.find_index(search_term) || "Not Found"
 end
+
+games = ["Super Mario Bros.", "Contra", "Metroid", "Mega Man 2"]
+puts search_index(games, "Contra")
