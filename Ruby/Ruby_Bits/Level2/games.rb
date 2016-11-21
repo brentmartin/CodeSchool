b9fe036600fdd3937b0e184d8e056b3af4c60248
Brent Martin <bmart35@gmail.com>
2016-11-20 23:08:49 -0600

    pass an array of arguments into a method

--- Ruby/Ruby_Bits/Level2/games.rb
+++ Ruby/Ruby_Bits/Level2/games.rb
@@ -1,14 +1,6 @@
-class InvalidGameError < StandardError; end
-def new_game(name, options={})
-  raise InvalidGameError, "You must provide a name for this game." unless name
-  {
-    name: name,
-    year: options[:year],
-    system: options[:system]
-  }
+def describe_favorites(games)
+  for game in games
+    puts "Favorite Game: #{game}"
+  end
 end
-begin
-  game = new_game(nil)
-rescue InvalidGameError => e
-  puts "There was a problem creating your new game: #{e.message}"
-end
+describe_favorites(['Mario', 'Contra', 'Metroid'])
