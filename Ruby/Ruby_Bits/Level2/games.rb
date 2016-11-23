20673d41adcb21db8d2d8d09e4e6ae0fbf3d0029
Brent Martin <bmart35@gmail.com>
2016-11-22 23:45:04 -0600

    can now access a game object through getter methods

--- Ruby/Ruby_Bits/Level2/games.rb
+++ Ruby/Ruby_Bits/Level2/games.rb
@@ -4,4 +4,16 @@ class Game
     @year = options[:year]
     @system = options[:system]
   end
+
+  def name
+    @name
+  end
+
+  def year
+    @year
+  end
+
+  def system
+    @system
+  end
 end
