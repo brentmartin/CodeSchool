a7851fe52717bc19ba179bc4de8355d773c8ad32
Brent Martin <bmart35@gmail.com>
2016-11-22 23:47:35 -0600

    use an attr_accessor to access attributes from the game object

--- Ruby/Ruby_Bits/Level2/games.rb
+++ Ruby/Ruby_Bits/Level2/games.rb
@@ -1,19 +1,9 @@
 class Game
+  attr_accessor :name, :year, :system
+
   def initialize(name, options={})
     @name = name
     @year = options[:year]
     @system = options[:system]
-  end
-
-  def name
-    @name
-  end
-
-  def year
-    @year
-  end
-
-  def system
-    @system
   end
 end
