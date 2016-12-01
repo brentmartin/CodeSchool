b42a85a0687121fd84ee2a655de0a71be8465479
Brent Martin <bmart35@gmail.com>
2016-12-01 00:00:01 -0600

    create a method that takes in an instance of a game and checks to see if it is accurate

--- Ruby/Ruby_Bits/Level3/Library.rb
+++ Ruby/Ruby_Bits/Level3/Library.rb
@@ -4,4 +4,11 @@ class Library
   def initialize(games)
     self.games = games
   end
+
+  def has_game?(search_game)
+    for game in games
+      return true if game == search_game
+    end
+    false
+  end
 end
